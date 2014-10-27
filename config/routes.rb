cbf3930bb5e804fb471e6ee94d216b074aaa438a
siobyrne <byrne.sio@gmail.com>
2014-10-27 03:32:40 -0400

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 RichsEstates::Application.routes.draw do
+  get 'sessions/new'
+
 	root							'static_pages#home'
   get  'about'	=>	'static_pages#about'
 	get  'signup' =>	'users#new'
+	get  'login'  =>	'sessions#new'
+	post 'login'	=>	'sessions#create'
+	delete 'logout' =>	'sessions#destroy'
 	resources :users
 	
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027042714) do
+ActiveRecord::Schema.define(version: 20141027065637) do
 
   create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "password_digest"
+    t.string   "password_digest", limit: 255
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
